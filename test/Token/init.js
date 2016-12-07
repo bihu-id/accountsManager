f611d18992aaa16792725e50def728ff2d4f59c9
Roy Luo <Roy@Roy-LuodeMacBook-Air.local>
2016-12-07 14:06:05 +0800

    deploy new contract

--- test/Token/init.js
+++ test/Token/init.js
@@ -3,10 +3,11 @@ contract('init tokenManager', function(accounts) {
 
     var tokenManager=address.tokonManagerdata
     var xindi=address.xindedata
+    var tokenporxy=address.tokenporxy
     it("tokenManager init", function(){
 
         var instance=TokenManager.at(tokenManager);
-        return instance.init(xindi,{from:accounts[0],gas:10000000}).then(function(tx){
+        return instance.init(xindi,tokenporxy,{from:accounts[0],gas:10000000}).then(function(tx){
             console.log(web3.eth.getTransactionReceipt(tx));
         })
     });
